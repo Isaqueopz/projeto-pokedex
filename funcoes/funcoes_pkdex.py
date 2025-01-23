5bd68aa4d02cc02b0073458f06e169e05b508009
Isaque Misael <sergioisaquepb@gmail.com>
2025-01-23 11:39:09 -0300

    foram adicionadas e corrigidas algumas funções (adicionadas as funções de printar o primeiro e ultimo pokemon. e a pesquisa por id, juntamente com o print do ID do pokemon )

--- funcoes/funcoes_pkdex.py
+++ funcoes/funcoes_pkdex.py
@@ -1,28 +1,64 @@
 from modulos import requests
 
-def pesquisar(nome_pokemon):
-    nome_pokemon = nome_pokemon.lower()  # A API exige o nome em minúsculo
-    url = f"https://pokeapi.co/api/v2/pokemon/{nome_pokemon}"
-    
+def pesquisar(nome_pokemon_ou_id):
+        nome_pokemon_ou_id = nome_pokemon_ou_id.lower()  # A API exige o nome em minúsculo
+        url = f"https://pokeapi.co/api/v2/pokemon/{nome_pokemon_ou_id}"
+        
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Levanta uma exceção para qualquer erro HTTP
+        except requests.exceptions.RequestException as err:
+            # Tratar qualquer erro de requisição (conexão, timeout, etc.)
+            return {"erro": f"Erro ao acessar a API: {err}"}
+
+        if response.status_code == 200:
+            dados = response.json()
+
+            pokemon_info = {
+                'nome': dados['name'],
+                'tipos': [tipo['type']['name'] for tipo in dados['types']],
+                'id': dados['id'],
+                'habilidade': [habilidade['ability']['name'] for habilidade in dados['abilities']],
+                'altura': dados['height'],
+                'peso': dados['weight']
+            }
+            return pokemon_info
+        else:
+            return {"erro": "Pokémon não encontrado ou houve um problema na requisição."}
+
+def printar_info_pokemon(info):
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # Levanta uma exceção para qualquer erro HTTP
-    except requests.exceptions.RequestException as err:
-        # Tratar qualquer erro de requisição (conexão, timeout, etc.)
-        return {"erro": f"Erro ao acessar a API: {err}"}
+        print(f"Nome: {info['nome']}")
+        print(f"(ID: {info['id']})")
+        print(f"Tipos: {', '.join(info['tipos'])}")
+        print(f"Habilidades: {', '.join(info['habilidade'])}")
+        print(f"Altura: {info['altura']} decímetros")
+        print(f"Peso: {info['peso']} hectogramas")
+    except KeyError:
+        print("Nome ou ID inválido!")
 
-    if response.status_code == 200:
-        dados = response.json()
-
-        pokemon_info = {
-            'nome': dados['name'],
-            'tipos': [tipo['type']['name'] for tipo in dados['types']],
-            'habilidade': [habilidade['ability']['name'] for habilidade in dados['abilities']],
-            'altura': dados['height'],
-            'peso': dados['weight']
-        }
-        return pokemon_info
+def printar_primeiro_pokemon_ou_ultimo(posicao):
+    if posicao == "primeiro":
+        info = pesquisar("1")
+        try:
+            print(f"Nome: {info['nome']}")
+            print(f"(ID: {info['id']})")
+            print(f"Tipos: {', '.join(info['tipos'])}")
+            print(f"Habilidades: {', '.join(info['habilidade'])}")
+            print(f"Altura: {info['altura']} decímetros")
+            print(f"Peso: {info['peso']} hectogramas")
+        except KeyError:
+            print("Nome ou ID inválido!")
     else:
-        return {"erro": "Pokémon não encontrado ou houve um problema na requisição."}
-
-
+        posicao == "ultimo"
+        info = pesquisar("1025")
+        try:
+            print(f"Nome: {info['nome']}")
+            print(f"(ID: {info['id']})")
+            print(f"Tipos: {', '.join(info['tipos'])}")
+            print(f"Habilidades: {', '.join(info['habilidade'])}")
+            print(f"Altura: {info['altura']} decímetros")
+            print(f"Peso: {info['peso']} hectogramas")
+        except KeyError:
+                print("Nome ou ID inválido!")
+    
