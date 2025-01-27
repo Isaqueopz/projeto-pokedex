76dad290df1c113f363435d8a55fd033ed74bb62
Isaque Misael <sergioisaquepb@gmail.com>
2025-01-27 15:46:47 -0300

    foi adicionada o chamamento da batalha de pokemons

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from funcoes.funcoes_pkdex import pesquisar, printar_info_pokemon,printar_primeiro_pokemon_ou_ultimo
+from funcoes.funcoes_pkdex import pesquisar, printar_info_pokemon,printar_primeiro_pokemon_ou_ultimo,batalha_pokemons
 
 # pokemon_nome = input("Digite o nome/id do pokemon: ")
 # info = pesquisar(pokemon_nome)
 # printar_info_pokemon(info)
-printar_primeiro_pokemon_ou_ultimo("ultimo")
+# printar_primeiro_pokemon_ou_ultimo("ultimo")
+batalha_pokemons(pokemon1='6',pokemon2='1025')
